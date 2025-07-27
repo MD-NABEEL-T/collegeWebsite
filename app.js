d029c352451ace137532f7d7a1fb478dcdc080ba
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-07-27 15:17:07 +0530

    added  contact page

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ closemenu = () => {
     sidemenu.style.right = "-200px";
 }
 
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+opentab = (event,tabname) =>{
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwV0kzBdoZU8OpiTB2dvtZTriHvuIYIyK5LIZ9Ox43ZsJi_RTlRc_QKXo_Q2i0IkAFeGQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  });
+
+
 let moreProjects = document.querySelector(".moreProjects");
 moreProjects.style.display="none";
 let seeMore = document.querySelector(".btn");
